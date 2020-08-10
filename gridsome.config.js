7545f18a24d07b1ee69d63a50271309a2b6735ea
Victor Valle <victor.valle@lkmx.io>
2020-08-10 03:39:51 -0700

    Home page structure complete

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,7 +4,48 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const path = require('path');
+
+function addStyleResource(rule) {
+  rule.use('style-resource').loader('style-resources-loader').options({
+    patterns: [
+      path.resolve(__dirname, './src/sass/_tokens.scss'),
+      path.resolve(__dirname,
+          './node_modules/@lkmx/flare/src/components/**/*.scss'),
+      path.resolve(__dirname,
+          './node_modules/@lkmx/flare/src/functions/**/*.scss'),
+      path.resolve(__dirname,
+          './node_modules/@lkmx/flare/src/guides/**/*.scss'),
+      path.resolve(__dirname,
+          './node_modules/@lkmx/flare/src/structures/**/*.scss'),
+    ],
+  });
+}
+
 module.exports = {
   siteName: 'MAAD lab',
-  plugins: []
-}
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'content/**/*.md',
+        typeName: 'Content',
+        remark: {
+          // remark options
+        },
+      },
+    },
+  ],
+  transformers: {
+    remark: {
+      // global remark options
+    },
+  },
+  chainWebpack(config) {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
+
+    types.forEach(type => {
+      addStyleResource(config.module.rule('scss').oneOf(type));
+    });
+  },
+};
---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <simple-layout>
+    <template #logo><g-link to="/"><g-image class="logo" src="../assets/flare-starter-logo--white.svg"></g-image></g-link></template>
+    <template #navigation>
+      <a href="#">Epics</a>
+      <a href="#">Stories</a>
+      <a href="#">Maps</a>
+    </template>
+    <template #actions>
+      <a href="#">Sign in</a>
+    </template>
+    <base-page>
+      <slot></slot>
+    </base-page>
+  </simple-layout>
+</template>
+
+<script>
+  export default {
+    name: 'BaseLayout',
+  };
+</script>
+
+<style>
+  .logo {
+    height: 30px;
+    width: auto;
+  }
+</style>

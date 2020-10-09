8ae2144d317cf7ce00c0186f1c9b794c97b12b86
Luis Espinoza <luis.espinoza@lkmx.io>
2020-10-08 21:42:49 -0700

    Add footer implementation

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -22,99 +22,207 @@
       <slot></slot>
     </base-page>
     <template #footer>
-      <single-column>
-        <p>This website has a secret.</p>
-        <p>info@secret.foundation</p>
+      <single-column class="custom-footer">
+        <div class="custom-footer__content">
+          <div>
+            <g-image src="../assets/secret-logo--white.svg" class="custom-footer__logo"></g-image>
+            <p>This website has a secret, contact us at</p>
+            <p><g-link to="mailto:info@secret.foundation">info@secret.foundation</g-link></p>
+            <div class="custom-footer__social">
+              <g-link><g-image src="../assets/discord.svg"></g-image></g-link>
+              <g-link><g-image src="../assets/telegram.svg"></g-image></g-link>
+              <g-link><g-image src="../assets/twitter.svg"></g-image></g-link>
+              <g-link><g-image src="../assets/youtube.svg"></g-image></g-link>
+            </div>
+          </div>
+          <div class="custom-footer__links">
+            <h4>Documentation</h4>
+            <g-link to="/developers/introduction/overview">Overview</g-link>
+            <g-link to="/developers/node-operators/validators/validators">For Validators</g-link>
+            <g-link to="/developers/secret-contract-devs/developing-secret-contracts">For Developers</g-link>
+          </div>
+          <div class="custom-footer__links">
+            <h4>Secret Community</h4>
+            <g-link to="https://blog.scrt.network/">Secret Blog</g-link>
+            <g-link to="https://chat.scrt.network/">Secret Chat</g-link>
+            <g-link to="https://forum.scrt.network/">Secret Forum</g-link>
+            <g-link to="/committees/introduction">Committees</g-link>
+          </div>
+          <div class="custom-footer__links">
+            <h4>Secret Network</h4>
+            <g-link to="/about/about-secret-network">About</g-link>
+            <g-link to="/about/faq">FAQ</g-link>
+          </div>
+        </div>
+        <div class="custom-footer__copy">
+          <span>© Secret Network. All Rights Reserved.</span>
+          <span><g-link>Terms of Service</g-link> | <g-link>Privacy Policy</g-link></span>
+        </div>
       </single-column>
     </template>
   </simple-layout>
 </template>
 
 <script>
-  export default {
-    name: 'BaseLayout',
-    data() {
-      return {
-        theme: null,
-        darkLightModeState: false, // false is dark
-        coloredModeState: false, // false is not activated
-      }
-    },
-    methods: {
-      toggleDarkLightMode() {
-        this.darkLightModeState = !this.darkLightModeState
-        this.setTheme()
-      },
-      toggleColoredMode() {
-        this.coloredModeState = !this.coloredModeState
-        this.setTheme()
-      },
-      setTheme() {
-        const [body] = document.getElementsByTagName('body')
-        if (!body) return
-        const colorMode = this.coloredModeState ? '-colored' : ''
-        const darkLightMode = this.darkLightModeState ? 'light' : 'dark'
-        const theme = `${darkLightMode}${colorMode}`
-        body.setAttribute('theme', theme)
-      }
+export default {
+  name: 'BaseLayout',
+  data() {
+    return {
+      theme: null,
+      darkLightModeState: false, // false is dark
+      coloredModeState: false, // false is not activated
     }
-  };
+  },
+  methods: {
+    toggleDarkLightMode() {
+      this.darkLightModeState = !this.darkLightModeState
+      this.setTheme()
+    },
+    toggleColoredMode() {
+      this.coloredModeState = !this.coloredModeState
+      this.setTheme()
+    },
+    setTheme() {
+      const [body] = document.getElementsByTagName('body')
+      if (!body) return
+      const colorMode = this.coloredModeState ? '-colored' : ''
+      const darkLightMode = this.darkLightModeState ? 'light' : 'dark'
+      const theme = `${darkLightMode}${colorMode}`
+      body.setAttribute('theme', theme)
+    }
+  }
+}
 </script>
 
 <style lang="scss">
-  .logo {
-    height: 58px;
-    width: auto;
-  }
-  body {
-    &[theme*=dark] {
-      .logo {
-        &[src*=black] {
-          display: none;
-        }
-        &[src*=white] {
-          display: block;
-        }
-      }
+.logo {
+  width: auto;
+  height: rem(58px);
+  &[src*=black] {
+    @include theme(dark dark-colored) {
+      display: none;
     }
-    &[theme*=light] {
-      .logo {
-        &[src*=black] {
-          display: block;
-        }
-        &[src*=white] {
-          display: none;
-        }
-      }
+    @include theme(light light-colored) {
+      display: block;
     }
   }
-  .theme-selector {
-    width: 18px;
-    height: 18px;
-    border-radius: 50%;
+  &[src*=white] {
+    @include theme(dark dark-colored) {
+      display: block;
+    }
+    @include theme(light light-colored) {
+      display: none;
+    }
+  }
+}
+.theme-selector {
+  width: rem(18px);
+  height: rem(18px);
+  border-radius: 50%;
+  border: 1px solid white;
+
+  &:not(:last-child) {
+    margin-right: 5px;
+  }
+
+  &--dark {
+    background-color: white;
+  }
+  &--light {
+    background-color: $primary-black-color;
+  }
+  &--no-colored {
+    background-color: gray;
     border: 1px solid white;
-    &:not(:last-child) {
-      margin-right: 5px;
+  }
+  &--colored {
+    background-color: #1B46FF;
+  }
+}
+.custom-footer {
+  &.single-column {
+    @include respond-to("large and up") {
+      padding: 0;
+    }
+    @include respond-to("medium and down") {
+      padding: 0 rem(16px);
+    }
+  }
+  p {
+    line-height: rem(25px);
+  }
+  &__logo {
+    width: rem(110px);
+    margin-bottom: rem(20px);
+  }
+  &__social {
+    margin-top: rem(16px);
+    display: grid;
+    grid-auto-flow: column;
+    grid-column-gap: rem(18px);
+    grid-auto-columns: min-content;
+    @include respond-to("medium and down") {
+      justify-content: center;
+      margin-bottom: rem(50px);
+    }
+  }
+  &__links {
+    display: grid;
+    grid-auto-flow: row;
+    grid-row-gap: rem(10px);
+    grid-auto-columns: max-content;
+    text-align: right;
+
+    @include respond-to("large and up") {
+      justify-content: end;
+      align-content: start;
+    }
+    @include respond-to("medium and down") {
+      justify-content: center;
+      text-align: center;
+      margin-bottom: rem(34px);
     }
 
-    &--dark {
-      background-color: white;
-    }
-    &--light {
-      background-color: $primary-black-color;
-    }
-    &--no-colored {
-      background-color: gray;
-      border: 1px solid white;
-    }
-    &--colored {
-      background-color: #1B46FF;
+    h4 {
+      font-weight: bold;
+      line-height: rem(25px);
+      margin-bottom: 6px; // Not the best option for spacing, but for now is fine...
     }
   }
-  .footer {
-    text-align: center;
-    & p:not(:last-child) {
-      margin-bottom: $gutter;
+  &__content {
+    display: grid;
+    padding: 0 rem(16px) rem(16px) rem(16px);
+
+    @include respond-to("large and up") {
+      grid-auto-flow: column;
+    }
+    @include respond-to("medium and down") {
+      justify-content: center;
+      text-align: center;
     }
   }
+  &__copy {
+    display: grid;
+    box-shadow: inset 0px 2px 0px #AAAAAA;
+    padding: rem(16px);
+
+    @include respond-to("large and up") {
+      grid-auto-flow: column;
+      grid-template-columns: 1fr 1fr;
+    }
+    @include respond-to("medium and down") {
+      grid-auto-flow: row;
+      justify-content: center;
+      text-align: center;
+      grid-row-gap: rem(12px);
+    }
+    span {
+      &:nth-of-type(2) {
+        @include respond-to("large and up") {
+          justify-self: end;
+        }
+      }
+    }
+  }
+}
 </style>

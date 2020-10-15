156e33f4e9bb9cd92a284afb4997e0afc206c1e9
Luis Espinoza <luis.espinoza@lkmx.io>
2020-10-15 11:00:22 -0700

    General minor adjustments

--- src/components/blog/LatestPosts.vue
+++ src/components/blog/LatestPosts.vue
@@ -1,6 +1,6 @@
 <template>
   <grid columns="3">
-    <blog-card v-for="{ node } in $static.posts.edges" :key="node.id">
+    <blog-card v-for="{ node } in $static.posts.edges" :key="node.id" :tag="node.primary_tag != null ? node.primary_tag.name : ''">
       <template #tag v-if="node.primary_tag">{{ node.primary_tag.name }}</template>
       <h4><g-link :to="`/blog/${node.slug}`">{{ node.title }}</g-link></h4>
       <separator small space-small></separator>

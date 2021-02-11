a4ffe32b13b2e43eff4ec5c71e35941400120c2a
Sandy Corsillo <the_dusky@hey.com>
2021-02-11 11:50:16 -0500

    calendar updates

--- gridsome.server.js
+++ gridsome.server.js
@@ -4,11 +4,36 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+const axios = require('axios');
+const moment = require('moment');
 
 module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
-    // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
-  })
+  api.loadSource(async actions => {
+    // // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
+    // const eventCollection = actions.addCollection('Events');
+    // const url = "https://content.googleapis.com/calendar/v3/calendars/primary/events?maxResults=10&orderBy=startTime&showDeleted=false&singleEvents=true&key=AIzaSyBKHAaOQqtd7_7upr_hAx1nCVJibhQI3vc"
+    // await axios.get(url).then(response => {
+    //   let events = response.result.items
+    //   events.forEach(event => {
+    //     eventCollection.addNode({
+    //       id: event.id,
+    //       name: event.summary,
+    //       startDate: event.start.dateTime,
+    //       endDate: event.end.dateTime
+    //     });
+    //   });
+    // });
+
+    const eventCollection = actions.addCollection('Events');
+
+    eventCollection.addNode({
+      id: 1,
+      name: "Test",
+      startDate: moment().subtract(1, 'days').format(),
+      endDate: moment().subtract(1, 'days').format()
+    });
+
+  });
 
   api.createPages(({ createPage }) => {
     // Use the Pages API here: https://gridsome.org/docs/pages-api/
--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -2,7 +2,7 @@
     <div class="calendar">
         <calendar-view
 			:show-date="showDate"
-            :items="items"
+            :items="$page.posts.edges"
 			class="theme-default holiday-us-traditional holiday-us-official">
 
 			<calendar-view-header
@@ -11,10 +11,10 @@
 				:header-props="t.headerProps"
 				@input="setShowDate" />
 		</calendar-view>
-    <button type="button" class="btn btn-primary" @click="refreshGApi">Refresh</button>
+    <!-- <button type="button" class="btn btn-primary" @click="refreshGApi">Refresh</button>
     <button type="button" class="btn btn-primary" v-if="!authorized" @click="handleAuthClick">Login</button>
     <button type="button" class="btn btn-primary" v-if="authorized" @click="handleSignOutClick">Sign Out</button>
-    <button type="button" class="btn btn-primary" v-if="authorized" @click="getEvents">Get Events</button>
+    <button type="button" class="btn btn-primary" v-if="authorized" @click="getEvents">Get Events</button> -->
     </div>
 </template>
 
@@ -22,10 +22,10 @@
 import { CalendarView, CalendarViewHeader } from "vue-simple-calendar"
 import moment from "moment"
 
-const CLIENT_ID = "932323359337-e5p4fh2dsutc9lte24gffchuhrdjfftl.apps.googleusercontent.com";
-const API_KEY = "AIzaSyBKHAaOQqtd7_7upr_hAx1nCVJibhQI3vc";
-const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'];
-const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
+// const CLIENT_ID = "932323359337-e5p4fh2dsutc9lte24gffchuhrdjfftl.apps.googleusercontent.com";
+// const API_KEY = "AIzaSyBKHAaOQqtd7_7upr_hAx1nCVJibhQI3vc";
+// const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'];
+// const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
 
 export default {
     components: {
@@ -35,9 +35,9 @@ export default {
     data() {
         return {
             showDate: new Date(),
-            items: [],
-            authorized: false,
-            gItems: [],
+            //items: $page.events.edges,
+            // authorized: false,
+            // gItems: [],
         }
     },
     created() {
@@ -47,62 +47,77 @@ export default {
         setShowDate(d) {
             this.showDate = d;
         },
-        refreshGApi() {
-            this.api = gapi;
-            this.handleClientLoad();
-        },
-        handleClientLoad() {
-            this.api.load('client:auth2', this.initClient);
-        },
-        initClient() {
-            let vm = this;
-            vm.api.client.init({
-                apiKey: API_KEY,
-                clientId: CLIENT_ID,
-                discoveryDocs: DISCOVERY_DOCS,
-                scope: SCOPES
-            }).then(_ => {
-                vm.api.auth2.getAuthInstance().isSignedIn.listen(vm.authorized);
-            });
-        },
+        // refreshGApi() {
+        //     this.api = gapi;
+        //     this.handleClientLoad();
+        // },
+        // handleClientLoad() {
+        //     this.api.load('client:auth2', this.initClient);
+        // },
+        // initClient() {
+        //     let vm = this;
+        //     vm.api.client.init({
+        //         apiKey: API_KEY,
+        //         clientId: CLIENT_ID,
+        //         discoveryDocs: DISCOVERY_DOCS,
+        //         scope: SCOPES
+        //     }).then(_ => {
+        //         vm.api.auth2.getAuthInstance().isSignedIn.listen(vm.authorized);
+        //     });
+        // },
 
-        handleAuthClick(event) {
-            Promise.resolve(this.api.auth2.getAuthInstance().signIn())
-            .then(_ => {
-                this.authorized = true;
-            });
-        },
-        handleSignOutClick(event) {
-            Promise.resolve(this.api.auth2.getAuthInstance().signOut())
-                .then(_ => {
-                    this.authorized = false;
-                });
-        },
-        getEvents() {
-            let vm = this;
-            vm.api.client.calendar.events.list({
-                'calendarId': 'primary',
-                'timeMin': moment().subtract(1, 'days').format(),
-                'showDeleted': false,
-                'singleEvents': true,
-                'maxResults': 10,
-                'orderBy': 'startTime'
-            }).then(response => {
-                this.gItems = response.result.items;
-                this.items = [];
-                for(let i=0; i < this.gItems.length; i++) {
-                    this.items.push({
-                        id: this.gItems[i].id,
-                        title: this.gItems[i].summary,
-                        startDate: this.gItems[i].start.dateTime,
-                        endDate: this.gItems[i].end.dateTime
-                    })
-                }
-            });
-        },
+        // handleAuthClick(event) {
+        //     Promise.resolve(this.api.auth2.getAuthInstance().signIn())
+        //     .then(_ => {
+        //         this.authorized = true;
+        //     });
+        // },
+        // handleSignOutClick(event) {
+        //     Promise.resolve(this.api.auth2.getAuthInstance().signOut())
+        //         .then(_ => {
+        //             this.authorized = false;
+        //         });
+        // },
+        // getEvents() {
+        //     let vm = this;
+        //     vm.api.client.calendar.events.list({
+        //         'calendarId': 'primary',
+        //         'timeMin': moment().subtract(1, 'days').format(),
+        //         'showDeleted': false,
+        //         'singleEvents': true,
+        //         'maxResults': 10,
+        //         'orderBy': 'startTime'
+        //     }).then(response => {
+        //         this.gItems = response.result.items;
+        //         this.items = [];
+        //         for(let i=0; i < this.gItems.length; i++) {
+        //             this.items.push({
+        //                 id: this.gItems[i].id,
+        //                 title: this.gItems[i].summary,
+        //                 startDate: this.gItems[i].start.dateTime,
+        //                 endDate: this.gItems[i].end.dateTime
+        //             })
+        //         }
+        //     });
+        // },
     }
 }
 </script>
+
+<page-query>
+{
+  events: allEvents {
+    edges {
+      node {
+        id
+        name
+        startDate
+        endDate
+      }
+    }
+  }
+}
+</page-query>
 
 <style lang="scss">
     .calendar {

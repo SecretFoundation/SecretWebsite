bc91ce877461f765e0be16bd3c66f74fa0475349
edith moreno <edith.moreno@lkmx.io>
2021-02-11 17:55:11 -0800

    Style properties added on Calendar.

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -78,10 +78,76 @@ export default {
 </static-query>
 
 <style lang="scss">
+    .outsideOfMonth {
+        opacity: 0;
+    }
     .calendar {
         display: flex;
         flex-direction: column;
         flex-grow: 1;
+    }
+    .cv-header {
+        border-style: none !important;
+        border-bottom: 1px solid #fff !important;
+        font-family: $headers-default-font;
+        font-size: 28px;
+        font-weight: 700;
+        position: relative;
+        padding-bottom: 34px;
+        button {
+            font-size: 28px !important;
+        }
+        &-nav {
+            width: 100%;
+            margin: 0;
+            .previousYear, .nextYear, .currentPeriod {
+                display: none;
+            }
+            .previousPeriod, .nextPeriod {
+                border: 0;
+            }
+            .nextPeriod {
+                position: absolute;
+                right: 0;
+            }
+        }
+        .periodLabel {
+            width: 100%;
+            position: absolute;
+            text-align: center;
+            display: unset !important;
+            z-index: -1;
+            font-size: 28px !important;
+        }
+        &-days {
+            display: none !important;
+        }
+    }
+    .cv-weeks {
+        grid-row-gap: 16px;
+        border: none !important;
+        margin-top: 50px;
+        .cv-week {
+            flex-basis: unset;
+        }
+        .cv-weekdays {
+            grid-column-gap: 16px;
+            .cv-day {
+                border: 2px solid #fff;
+                border-radius: 10px;
+                padding: 10px;
+                height: 166px;
+                position: relative;
+                &-number {
+                    font-size: 22px;
+                    font-weight: 700;
+                    font-family: $headers-default-font;
+                    position: absolute;
+                    top: 16px;
+                    right: 16px;
+                }
+            }
+        }
     }
 
 

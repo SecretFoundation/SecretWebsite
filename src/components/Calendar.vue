f9009a364a90fb8d429a9ca6962aeac70decbdad
edith moreno <edith.moreno@lkmx.io>
2021-02-12 17:02:55 -0800

    Style updates on Calendar.

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -88,16 +88,17 @@ export default {
         }
         &-nav {
             width: 100%;
-            margin: 0;
-            .previousYear, .nextYear, .currentPeriod {
+            margin: 0 !important;
+            display: flex;
+            justify-content: space-between;
+            .previousYear, .nextYear {
                 display: none;
+            }
+            .currentPeriod {
+                opacity: 0;
             }
             .previousPeriod, .nextPeriod {
                 border: 0;
-            }
-            .nextPeriod {
-                position: absolute;
-                right: 0;
             }
         }
         .periodLabel {
@@ -107,6 +108,8 @@ export default {
             display: unset !important;
             z-index: -1;
             font-size: 28px !important;
+            padding: .4355em 0 !important;
+            margin: 0 !important;
         }
         &-days {
             display: none !important;
@@ -125,15 +128,15 @@ export default {
                 border: 2px solid #fff;
                 border-radius: 10px;
                 padding: 10px;
-                height: 166px;
-                position: relative;
+                min-height: 166px;
+                //height: auto;
+                //position: relative;
                 &-number {
                     font-size: 22px;
                     font-weight: 700;
                     font-family: $headers-default-font;
-                    position: absolute;
-                    top: 16px;
-                    right: 16px;
+                    width: 100%;
+                    text-align: right;
                 }
             }
         }
@@ -141,6 +144,74 @@ export default {
 
 
     .cv-item {
-        color: black;
+        color: white;
+        border-radius: 2px;
+        padding: 4px;
+        margin-top: 30px;
+        text-overflow: ellipsis;
+        border: 0;
+        border-style: none !important;
+        font-weight: bold;
+        font-size: 12px;
+        line-height: 12px;
+        width: calc((100% / 7) - 38px) !important;
+        display: flex;
+        flex-direction: column;
+        &:last-child {
+            background-color: green;
+        }
+        &.offset0 {
+            left: 13px !important;
+        }
+
+        &.offset1 {
+            left: calc((100% / 7) + 15px) !important;
+        }
+
+        &.offset2 {
+            left: calc(((100% / 7) * 2) + 17px) !important;
+        }
+
+        &.offset3 {
+            left: calc(((100% / 7) * 3) + 19px) !important;
+        }
+
+        &.offset4 {
+            left: calc(((100% / 7) * 4) + 21px) !important;
+        }
+
+        &.offset5 {
+            left: calc(((100% / 7) * 5) + 23px) !important;
+        }
+
+        &.offset6 {
+            left: calc(((100% / 7) * 6) + 25px) !important;
+        }
+        .startTime {
+            font-weight: normal;
+            order: 3;
+        }
+        .endTime {
+            display: none;
+        }
+    }
+    .cv-item[style*="calc(1.4em + 0px)"] {
+        background-color: #816DA8 !important;
+    }
+    .cv-item[style*="calc(2.8em + 2px)"] {
+        background-color: $primary-orange-color !important;
+        top: calc(2.8em + 20px) !important;
+    }
+    .cv-item[style*="calc(4.2em + 4px)"] {
+        background-color: $primary-purple-color !important;
+        top: calc(4.2em + 40px) !important;
+    }
+    .cv-item[style*="calc(5.6em + 6px)"] {
+        background-color: #5AA361 !important;
+        top: calc(5.6em + 60px) !important;
+    }
+    .cv-item[style*="calc(7em + 8px)"] {
+        background-color: $primary-blue-color !important;
+        top: calc(7em + 80px) !important;
     }
 </style>

b3d2fa01dd5d53b7aca5e5012f730ca9b01f07ca
Sandy Corsillo <the_dusky@hey.com>
2021-02-16 11:35:24 -0500

    added ver file

--- gridsome.server.js
+++ gridsome.server.js
@@ -16,20 +16,40 @@ module.exports = function (api) {
   api.loadSource(async actions => {
 
     async function getEvents(client) {
-      let eventCollection;
+      function uuidv4() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+          var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+          return v.toString(16);
+        });
+      }
+
+      const UUID = uuidv4();
+
+
       const calendar = google.calendar({ version: 'v3', auth: client});
+
+      calendar.events.watch(
+        {
+          auth: client,
+          resource: {
+            id: UUID,
+            type: "web_hook",
+            address: "https://secret-website-development.onrender.com/rebuild"
+          },
+          calendarId: 'sandy@stakeordie.com'
+        },(error, response) => {
+          if (error) {
+              console.log(error);
+              return;
+          }
+          console.log(response);
+        }
+      );
       
-      /*calendar.events.watch({
-        calendarId: 'sandy@stakeordie.com',
-        id: "123632432456",
-        type: "web_hook",
-        address: "https://secret-website-development.onrender.com/rebuild"
-      },(err, res) => {
-        console.log(err);
-        //console.log(res);
-      });*/
+      let eventCollection;
 
       return new Promise((resolve, reject) => {
+
         calendar.events.list(
           {
             calendarId: 'sandy@stakeordie.com',
@@ -42,6 +62,7 @@ module.exports = function (api) {
             if (err) {
               console.log(`The API returned an error: ${err}`)
             }
+
             const eventCollection = actions.addCollection('Events');
             
             res.data.items.forEach(item => {
